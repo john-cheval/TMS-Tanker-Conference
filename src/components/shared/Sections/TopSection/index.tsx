68276535b28304e1818a3245e9b93d6fa9418db0
john-cheval <johncheval1@gmail.com>
2025-09-19 17:00:56 +0400

    added the data

--- src/components/shared/Sections/TopSection/index.tsx
+++ src/components/shared/Sections/TopSection/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import isVideo from "@/lib/CheckIsVideo";
 import Image from "next/image";
+import imageLocal from "@/assets/sponsorOpp/sponsorOpp.png";
 
 interface SharedTopSectionPropsTypes {
   banner?: string;
@@ -13,7 +14,8 @@ const SharedTopSection = ({ banner, title }: SharedTopSectionPropsTypes) => {
       <div className="relative ">
         {!isVideo(banner ?? "") ? (
           <Image
-            src={banner ?? ""}
+            // src={banner ?? imageLocal}
+            src={imageLocal}
             width={500}
             height={400}
             alt="title"
@@ -33,7 +35,7 @@ const SharedTopSection = ({ banner, title }: SharedTopSectionPropsTypes) => {
 
       <div
         className="section-wrapper py-5 md:py-6  lg:py-8 bg-[linear-gradient(89deg,rgba(34,51,79,0.8)0.99%,rgba(0,138,192,0)97.36%)]
-    backdrop-blur-[7.15px] absolute -bottom-1 z-50 w-full 2xl:w-[75%] lg:w-[85%] clip-path-slant-banners"
+    backdrop-blur-[7.15px] absolute bottom-0 z-50 w-full 2xl:w-[75%] lg:w-[85%] clip-path-slant-banners"
       >
         <h1 className="main-heading font-bold text-center md:text-left leading-3 md:leading-[40px]  lg:leading-1 text-white">
           {title}
