14d4858291193c7660b03c9601c86971b6acd675
john-cheval <johncheval1@gmail.com>
2025-09-26 10:19:26 +0400

    why attend page has been done

--- src/components/why-attend/Section1/index.tsx
+++ src/components/why-attend/Section1/index.tsx
@@ -7,23 +7,6 @@ type Props = {
   data?: WhyAttendListType[];
 };
 
-const colors = [
-  { bgColor: "#662D91", headingColor: "#fff", textcolor: "#fff" },
-  { bgColor: "#0078BB", headingColor: "#fff", textcolor: "#fff" },
-  { bgColor: "#0C1E23", headingColor: "#fff", textcolor: "#fff" },
-  {
-    bgColor: "#EDF9FF",
-    headingColor: "linear-gradient(249deg, #4D1592 13.95%, #0078BB 80.04%)",
-    textcolor: "#000",
-  },
-];
-
-const isLastColor = {
-  bgColor: "linear-gradient(249deg, #4D1592 13.95%, #0078BB 80.04%)",
-  headingColor: "#fff",
-  textcolor: "#fff",
-};
-
 const WhyAttendSectionOne = ({ data }: Props) => {
   return (
     <div className=" mt-2 md:mt-8 lg:mt-11 section-wrapper space-y-3 md:space-y-4 xl:space-y-5">
@@ -32,22 +15,14 @@ const WhyAttendSectionOne = ({ data }: Props) => {
         data?.map((item, index) => {
           const isOdd = index % 2 === 0 || index === 0;
 
-          let currentColor;
-          if (index === data.length - 1) {
-            currentColor = isLastColor;
-          } else {
-            const colorIndex = index % colors.length;
-            currentColor = colors[colorIndex];
-          }
-
           return (
             <div
               key={index + 1}
-              className={`grid grid-cols-12 gap-y-3 md:gap-x-4 xl:gap-x-5`}
+              className={`grid grid-cols-12 gap-y-3 md:gap-x-4 xl:gap-x-5 border gradient-border-image `}
             >
               {isOdd ? (
                 <>
-                  <div className="col-span-12 md:col-span-4 responsive-radius overflow-hidden flex">
+                  <div className="col-span-12 md:col-span-4  overflow-hidden flex">
                     {!isVideo(item?.image_url ?? "") ? (
                       <Image
                         src={item?.image_url ?? ""}
@@ -67,63 +42,29 @@ const WhyAttendSectionOne = ({ data }: Props) => {
                       />
                     )}
                   </div>
-                  <div
-                    className="col-span-12 md:col-span-8 px-6 py-8 md:p-9 lg:p-12 2xl:p-14 responsive-radius flex flex-col gap-y-2 justify-center"
-                    style={{ background: currentColor.bgColor }}
-                  >
+                  <div className="col-span-12 md:col-span-8 px-6 py-8 md:p-9 lg:p-12 2xl:p-14  flex flex-col gap-y-2 justify-center">
                     {item?.title && (
-                      <h4
-                        className="main-heading-2"
-                        style={{
-                          background: currentColor.headingColor,
-                          backgroundClip: "text",
-                          WebkitBackgroundClip: "text",
-                          color: "transparent",
-                          WebkitTextFillColor: "transparent",
-                        }}
-                      >
+                      <h4 className="main-heading gradient-text leading-3 font-bold md:leading-[40px]  lg:leading-main w-fit ">
                         {item?.title}
                       </h4>
                     )}
 
-                    <p
-                      className="description"
-                      style={{
-                        color: currentColor.textcolor,
-                      }}
-                    >
+                    <p className="description text-black">
                       {item?.description}
                     </p>
                   </div>
                 </>
               ) : (
                 <>
-                  <div
-                    className="col-span-12 md:col-span-8 px-6 py-8 md:p-9 lg:p-12 2xl:p-14 responsive-radius flex flex-col gap-y-2 justify-center order-2 md:order-1"
-                    style={{ background: currentColor.bgColor }}
-                  >
-                    <h4
-                      className="main-heading-2"
-                      style={{
-                        color: currentColor.headingColor,
-                      }}
-                    >
+                  <div className="col-span-12 md:col-span-8 px-6 py-8 md:p-9 lg:p-12 2xl:p-14  flex flex-col gap-y-2 justify-center order-2 md:order-1">
+                    <h4 className="main-heading gradient-text leading-3 font-bold md:leading-[40px]  lg:leading-main w-fit">
                       {item?.title}
                     </h4>
-                    <p
-                      className="description"
-                      style={{
-                        background: currentColor.headingColor,
-                        backgroundClip: "text",
-                        WebkitBackgroundClip: "text",
-                        color: "transparent",
-                        WebkitTextFillColor: "transparent",
-                      }}
-                    >
+                    <p className="description text-black">
                       {item?.description}
                     </p>
                   </div>
-                  <div className="col-span-12 md:col-span-4 responsive-radius flex  overflow-hidden order-1 md:order-2">
+                  <div className="col-span-12 md:col-span-4  flex  overflow-hidden order-1 md:order-2">
                     {!isVideo(item?.image_url ?? "") ? (
                       <Image
                         src={item?.image_url ?? ""}
